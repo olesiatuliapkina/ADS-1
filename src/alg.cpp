449a5df160df9041dbf5ae1fb9941611e7771452
olesiatuliapkina <151723481+olesiatuliapkina@users.noreply.github.com>
2024-03-01 23:17:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,47 +4,42 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value <= 1) {
+  if (value < 2) {
     return false;
-  } else if (value > 1) {
-    for (int k = 2; k < value; k++) {
-      if (value % k == 0) {
-        return false;
-      } else {
-        return true;
-      }
+  }
+  for (int k = 2; k < value; k++) {
+    if (value % k == 0) {
+      return false;
     }
   }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  uint64_t number = 1;
   uint64_t counter = 0;
+  uint64_t number = 2; 
   while (counter < n) {
-    number++;
     if (checkPrime(number)) {
       counter++;
-      }
+    }
     number++;
   }
-  return number;
+  return number - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  value++;
+  uint64_t number = value + 1;
   while (true) {
-    if (checkPrime(value)) {
-      return value;
-      } else {
-      return value++;
+    if (checkPrime(number)) {
+      return number;
     }
+    number++;
   }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for (uint64_t k = 2; k <= hbound; k++) {
+  for (int k = 2; k < hbound; k++) {
     if (checkPrime(k)) {
       sum += k;
     }
